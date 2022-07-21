b48479608ac00b580b34b90ecf0b01648279ff72
GuiMoraes10 <oguimoraes721@gmail.com>
2022-07-20 22:42:44 -0300

    Projeto completo

---
+++ src/app/components/bookstore-app/product-list/product-item/product-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Game } from '../model/Game';
+
+@Component({
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.css']
+})
+export class ProductItemComponent implements OnInit {
+
+  @Input()
+  game!:Game;
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/bookstore-app/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { GameService } from './product-list.component.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  
+
+  games!: any;
+  gameService:GameService;
+
+  constructor(gameService:GameService) { 
+    this.gameService = gameService;
+  }
+
+  ngOnInit(): void {
+    this.games = this.gameService.getGame();
+      console.log(this.games);
+    }
+  }
+
+
